f6071a4933d110794d0b0b03eedcd7d7d0a5f3f4
MrTrollNugNug <cazawadzki@comcast.net>
2016-11-21 21:14:46 -0500

    Added Basic Classes to GitHub

---
+++ 1.10.2/src/main/java/com/mrtrollnugnug/xpBlocker/EventManager.java
@@ -0,0 +1,86 @@
+package com.mrtrollnugnug.xpBlocker;
+
+import net.minecraft.entity.item.EntityXPOrb;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemSmeltedEvent;
+
+public class EventManager {
+	@SubscribeEvent
+	public void onEntityJoinWorld(EntityJoinWorldEvent event){
+		if (event.getEntity() instanceof EntityXPOrb){
+			if (!event.getEntity().getEntityData().hasKey("XPBLOCKER_protected")){
+				event.setCanceled(true);
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onPlayerUseBlock(PlayerInteractEvent.RightClickBlock event){
+		if (event.getItemStack() != null){
+			if (event.getItemStack().getItem() == Items.EXPERIENCE_BOTTLE){
+				event.getItemStack().stackSize --;
+				int xp = 5;
+				for (int i = 0; i < xp; i ++){
+					if (!event.getWorld().isRemote){
+						EntityXPOrb orb = new EntityXPOrb(event.getWorld(), event.getPos().getX()+0.5, event.getPos().getY()+0.5, event.getPos().getZ()+0.5, 2);
+						orb.getEntityData().setBoolean("XPBLOCKER_protected", true);
+						event.getWorld().spawnEntityInWorld(orb);
+					}
+				}
+				event.setCanceled(true);
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onPlayerUse(PlayerInteractEvent.RightClickItem event){
+		if (event.getItemStack() != null){
+			if (event.getItemStack().getItem() == Items.EXPERIENCE_BOTTLE){
+				int xp = 5;
+				for (int i = 0; i < xp; i ++){
+					if (!event.getWorld().isRemote){
+						EntityXPOrb orb = new EntityXPOrb(event.getWorld(), event.getPos().getX()+0.5, event.getPos().getY()+0.5, event.getPos().getZ()+0.5, 2);
+						orb.getEntityData().setBoolean("XPBLOCKER_protected", true);
+						event.getWorld().spawnEntityInWorld(orb);
+					}
+				}
+				event.setCanceled(true);
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onBlockBreak(BreakEvent event){
+		if (event.getExpToDrop() > 0){
+			int xp = event.getExpToDrop();
+			for (int i = 0; i < xp; i ++){
+				if (!event.getWorld().isRemote){
+					EntityXPOrb orb = new EntityXPOrb(event.getWorld(), event.getPos().getX()+0.5, event.getPos().getY()+0.5, event.getPos().getZ()+0.5, 2);
+					orb.getEntityData().setBoolean("XPBLOCKER_protected", true);
+					event.getWorld().spawnEntityInWorld(orb);
+				}
+			}
+			event.setExpToDrop(0);
+		}
+	}
+	
+	@SubscribeEvent
+	public void onSmelt(ItemSmeltedEvent event){
+		if (isOre(event.smelting)){
+			int xp = (int) (event.smelting.getItem().getSmeltingExperience(event.smelting)*event.smelting.stackSize);
+			event.player.addExperience(xp);
+		}
+	}
+	
+	public boolean isOre(ItemStack stack){
+		if (Main.containsMatch(true, Main.oreItems, stack)){
+			return true;
+		}
+		return false;
+	}
+}
---
+++ 1.10.2/src/main/java/com/mrtrollnugnug/xpBlocker/Main.java
@@ -0,0 +1,58 @@
+package com.mrtrollnugnug.xpBlocker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameData;
+import net.minecraftforge.oredict.OreDictionary;
+
+@Mod(modid = Main.MODID, version = Main.VERSION, name = Main.MODNAME)
+public class Main
+{
+    public static final String MODID = "xpblocker";
+    public static final String VERSION = "1.3";
+    public static final String MODNAME = "XP Blocker";
+    
+    static ArrayList<ItemStack> oreItems = new ArrayList<ItemStack>();
+    
+    @EventHandler
+    public static void preInit(FMLPreInitializationEvent event){
+    	MinecraftForge.EVENT_BUS.register(new EventManager());
+    }
+    
+    @EventHandler
+    public static void postInit(FMLPostInitializationEvent event){
+    	for (int i = 0; i < GameData.getItemRegistry().getKeys().size(); i ++){
+    		ItemStack stack = new ItemStack((Item)GameData.getItemRegistry().getObject((ResourceLocation) GameData.getItemRegistry().getKeys().toArray()[i]));
+    		int[] ids = OreDictionary.getOreIDs(stack);
+    		System.out.println("ids"+ids);
+    		if (ids.length > 0){
+    			for (int j = 0; j < ids.length; j ++){
+    				if (OreDictionary.getOreName(ids[j]).length() > 3){
+    					if (OreDictionary.getOreName(ids[j]).substring(0, 3).equals("ore")){
+	    					oreItems.add(FurnaceRecipes.instance().getSmeltingResult(stack));
+	    				}
+    				}
+    			}
+    		}
+    	}
+    }
+    
+    public static boolean containsMatch(boolean strict, List<ItemStack> items, ItemStack stack){
+    	for (int i = 0; i < items.size(); i ++){
+    		if (OreDictionary.itemMatches(items.get(i), stack, strict)){
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+}
